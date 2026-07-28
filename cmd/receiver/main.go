56d4ebb7598058c4e07c2f9b416b99273065df7c
adolp26 <adolpheribeiro7@gmail.com>
2026-07-28 18:38:26 -0300

    init

---
+++ cmd/receiver/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+
+	listener, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		fmt.Println("Error starting TCP server:", err)
+		return
+	}
+	defer listener.Close()
+
+	fmt.Println("TCP server listening on port 9000")
+
+	conn, err := listener.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection:", err)
+		return
+	}
+	defer conn.Close()
+
+	fmt.Println("Connection accepted from", conn.RemoteAddr())
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/adolp26/nearcast
+
+go 1.24.4
