40a54ac53aa90b4a6857c24e7533a3f3612249db
adolp26 <adolpheribeiro7@gmail.com>
2026-09-02 10:33:49 -0300

    feat(receiver): add http methods

--- cmd/receiver/main.go
+++ cmd/receiver/main.go
@@ -1,83 +1,48 @@
 package main
 
 import (
-	"encoding/binary"
 	"fmt"
 	"io"
-	"net"
+	"net/http"
 	"os"
 )
 
-func handleConnection(conn net.Conn, id int) {
-	defer conn.Close()
-
-	tamanhoBuf := make([]byte, 4)
-
-	_, err := io.ReadFull(conn, tamanhoBuf)
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	file, header, err := r.FormFile("file")
 	if err != nil {
-		fmt.Println("Error reading file size:", err)
-		return
-	}
-
-	tamanhoNome := binary.BigEndian.Uint32(tamanhoBuf)
-
-	nomeBuf := make([]byte, tamanhoNome)
-
-	_, err = io.ReadFull(conn, nomeBuf)
-	if err != nil {
-		fmt.Println("Error reading file name:", err)
-		return
-	}
-
-	nomeArquivo := string(nomeBuf)
-
-	fmt.Println("Receiving file:", nomeArquivo)
-
-	tamanhoArquivoBuf := make([]byte, 4)
-	_, err = io.ReadFull(conn, tamanhoArquivoBuf)
-	if err != nil {
-		fmt.Println("Error reading file size:", err)
-		return
-	}
-
-	tamanhoArquivo := binary.BigEndian.Uint32(tamanhoArquivoBuf)
-
-	file, err := os.Create(nomeArquivo)
-	if err != nil {
-		fmt.Println("Error creating file:", err)
+		fmt.Println("Error uploading file:", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 	defer file.Close()
 
-	_, err = io.CopyN(file, conn, int64(tamanhoArquivo))
+	nomeArquivo := header.Filename
+	fmt.Println("Receiving file:", nomeArquivo, "size:", header.Size)
+
+	dst, err := os.Create(nomeArquivo)
 	if err != nil {
-		fmt.Println("Error receiving file:", err)
+		fmt.Println("Error creating file:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	fmt.Println("Transfer finished for connection", id)
+	defer dst.Close()
+
+	_, err = io.Copy(dst, file)
+	if err != nil {
+		fmt.Println("Error saving file:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Println("Transfer finished:", nomeArquivo)
+	fmt.Fprintf(w, "File %s uploaded successfully", nomeArquivo)
 }
 
 func main() {
-	listener, err := net.Listen("tcp", ":9000")
+	http.HandleFunc("/upload", uploadHandler)
+	fmt.Println("Http server listening on port 9000")
+
+	err := http.ListenAndServe(":9000", nil)
 	if err != nil {
-		fmt.Println("Error starting TCP server:", err)
-		return
-	}
-	defer listener.Close()
-
-	fmt.Println("TCP server listening on port 9000")
-
-	id := 0
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection:", err)
-			continue
-		}
-
-		fmt.Println("Connection accepted from", conn.RemoteAddr())
-
-		id++
-		go handleConnection(conn, id)
+		fmt.Println("Error starting server:", err)
 	}
 }
